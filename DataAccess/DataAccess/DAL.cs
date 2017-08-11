77958e29a88efc5ccc37840a2c831666a09acfad
Phil Scott <pscott@iqms.com>
2017-08-11 02:20:29 -0400

    Master commit

---
+++ DataAccess/DataAccess/DAL.cs
@@ -0,0 +1,49 @@
+﻿using Objects;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DAL
+    {
+        OracleConnection _conn;
+        string _connString = "Data Source=(DESCRIPTION="
+             + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=PSCOTT-1)(PORT=1521)))"
+             + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=EATMETOO)));"
+             + "User Id=eatme;Password=eatme;";
+        public DAL()
+        {
+            _conn = new OracleConnection(_connString);
+        }
+
+        public User GetUser(string userName,string password)
+        {
+            User user = new User();
+            _conn.Open();
+
+            string sql = string.Format("select id,first_name,last_name from eatme_users where email = '{0}' and password = '{1}' ",userName,password); 
+            OracleCommand cmd = new OracleCommand(sql, _conn);
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                
+                user.Id = int.Parse(dr["id"].ToString());
+                user.FirstName = dr["first_name"].ToString();
+                user.LastName = dr["last_name"].ToString();
+                
+            }
+            
+            _conn.Close();
+
+            return user;
+
+        }
+    }
+}
---
+++ EatMeWeb/Login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="EatMeWeb._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <br /><br />
+   
+    <img src="./Content/images/EatMe.png" height="125" />
+    <p>
+        <br />
+    </p>
+    <p>
+        Login Name</p>
+    <p>
+        <asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Password</p>
+    <p>
+        <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" />
+    </p>
+   
+
+</asp:Content>
